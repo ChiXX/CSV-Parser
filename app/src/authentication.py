d9260ac18d936ca42c7b0faf20f3ca475ac01919
Xu CHi <1436160465@qq.com>
2023-08-12 18:06:28 +0200

    flash messages

--- app/src/authentication.py
+++ app/src/authentication.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 authentication: Blueprint = Blueprint("authentication", __name__)
 
 
 @authentication.route("/login", methods=["GET", "POST"])
 def login() -> str:
-    return render_template("login.html", text='test')
+    return render_template("login.html", text="test")
 
 
 @authentication.route("/logout")
@@ -15,4 +15,23 @@ def logout() -> str:
 
 @authentication.route("/sign-up", methods=["GET", "POST"])
 def sing_up() -> str:
+    if request.method == "POST":
+        email: str = request.form.get("email")
+        first_name: str = request.form.get("firstName")
+        password1: str = request.form.get("password1")
+        password2: str = request.form.get("password2")
+
+        # user = User.query.filter_by(email=email).first()
+        if False:
+            flash("Email already exists.", category="error")
+        elif len(email) < 4:
+            flash("Email must be greater than 3 characters.", category="error")
+        elif len(first_name) < 2:
+            flash("First name must be greater than 1 character.", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match.", category="error")
+        elif len(password1) < 4:
+            flash("Password must be at least 4 characters.", category="error")
+        else:
+            pass
     return render_template("signup.html")
