ed2e2a62a0288ad53512f6e0206201713421a770
Xu CHi <1436160465@qq.com>
2023-08-13 16:01:03 +0200

    validation file

--- app/src/homepage.py
+++ app/src/homepage.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, Response, flash, render_template, request
+from flask import (
+    Blueprint,
+    Response,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
-from app.src.util import allowed_file
+from app.src.util import get_csv_content, has_mandatory_columns, is_allowed_suffix
 
 
 homepage: Blueprint = Blueprint("homepage", __name__)
@@ -12,16 +20,28 @@ homepage: Blueprint = Blueprint("homepage", __name__)
 def home() -> str | Response:
     if request.method == "POST":
         file = request.files["file"]
-        if file and allowed_file(file.filename):
+        if file:
+            print(is_allowed_suffix(file.filename))
             filename = secure_filename(file.filename)
-            flash(f'{filename} uploaded!', category='success')
-            # return render_template("homepage.html", user=current_user, file=file)
-            # new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
-            
+            if not is_allowed_suffix(file.filename):
+                print(file.content_length)
+                flash(f'Invalid file type: {filename.split(".")[1]}', category="error")
+            else:
+                content = get_csv_content(file)
+                if not has_mandatory_columns(content[0]):
+                    flash("Missing mandatory column", category="error")
+                else:
+                    return redirect(url_for("homepage.home", user=current_user))
 
-            # output_file = process_csv(save_location)
-            # return send_from_directory('output', output_file)
-            # return redirect(url_for("download"))
+        # if file and allowed_file(file.filename):
+        #     flash(f'{filename} uploaded!', category='success')
+
+        # return render_template("homepage.html", user=current_user, file=file)
+        # new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
+
+        # output_file = process_csv(save_location)
+        # return send_from_directory('output', output_file)
+        # return redirect(url_for("download"))
     return render_template("homepage.html", user=current_user)
 
 
--- app/src/util.py
+++ app/src/util.py
@@ -1,33 +1,64 @@
+import codecs
 import csv
 import os
 from collections import defaultdict
 from datetime import datetime
 from decimal import Decimal
+from werkzeug.datastructures.file_storage import FileStorage
 
-def process_csv(filename: str)-> str:
+
+def process_csv(filename: str) -> str:
     product_types = defaultdict(Decimal)
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         reader = csv.DictReader(f)
         for row in reader:
             try:
-                hypen_idx = row['product'].replace('"', '').split().index('-')
-                product_type =  ' '.join(row['product'].split()[:hypen_idx])
-                product_types[product_type] += Decimal(row['price'][1:])
+                hypen_idx = row["product"].replace('"', "").split().index("-")
+                product_type = " ".join(row["product"].split()[:hypen_idx])
+                product_types[product_type] += Decimal(row["price"][1:])
             except ValueError:
-                product_types[row['product'].replace('"', '')] += Decimal(row['price'][1:])
+                product_types[row["product"].replace('"', "")] += Decimal(
+                    row["price"][1:]
+                )
 
-    output_file = f'product_types_{str(datetime.now())}.csv'
-    with open(os.path.join('output', output_file), 'w') as f:
+    output_file = f"product_types_{str(datetime.now())}.csv"
+    with open(os.path.join("output", output_file), "w") as f:
         writer = csv.writer(f)
-        writer.writerow(['product_type', 'price'])
+        writer.writerow(["product_type", "price"])
 
         for product_type, price in product_types.items():
             writer.writerow([product_type, price])
 
     return output_file
 
-ALLOWED_EXTENSIONS: set[str] = set(["csv"])
 
-def allowed_file(filename: str) -> bool:
+ALLOWED_EXTENSIONS: set[str] = set(["csv", "tsv"])
+MANDATORY_COLUMNS: list[str] = [
+    "chrom1",
+    "start1",
+    "end1",
+    "chrom2",
+    "end2",
+    "sample",
+    "score",
+]
+
+
+def is_allowed_suffix(filename: str) -> bool:
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_csv_content(file: FileStorage) -> list[list[str]]:
+    stream = codecs.iterdecode(file.stream, "utf-8")
+    rows = csv.reader(stream, dialect=csv.excel)
+    content = []
+    for row in rows:
+        content.append(row) 
+    return row
+
+def has_mandatory_columns(headers: list[str]) -> bool:
+    for title in MANDATORY_COLUMNS:
+        if title not in headers:
+            return False
+    return True
