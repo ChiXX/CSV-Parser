e70183c2035da2a38b9aeee3b602524a3f180b3f
Xu CHi <1436160465@qq.com>
2023-08-15 01:52:53 +0200

    table functions

--- app/src/homepage.py
+++ app/src/homepage.py
@@ -13,52 +13,123 @@ homepage: Blueprint = Blueprint("homepage", __name__)
 
 
 class FileData:
-    def __init__(self, file, headers, contents, by=""):
-        self.file = file
-        self.headers = headers
-        self.contents = contents
-        self.by = by
-        self.all_contents = contents
+    sort_by_options = [
+        "---",
+        "chrom1",
+        "start1",
+        "end1",
+        "chrom2",
+        "start2",
+        "end2",
+        "sample",
+        "score",
+    ]
+    group_by_options = ["---", "chrom1", "chrom2", "sample"]
+    show_top_options = [5, 10, 15, 20]
 
-    def sort_by(self, by):
-        self.by = by
-        if by == "chrom1":
+    def __init__(
+        self,
+        file,
+        contents,
+        sort_by_option="---",
+        group_by_option="---",
+        show_top_option=10,
+    ):
+        self.file = file
+        self.contents = contents
+        self.all_contents = contents
+        self.sort_by_option = sort_by_option
+        self.group_by_option = group_by_option
+        self.show_top_option = show_top_option
+
+    def apply_settings_to_content(
+        self,
+        sort_by_option="---",
+        group_by_option="---",
+        show_top_option=10,
+    ):
+        self.group_by_option = group_by_option
+        self.sort_by_option = sort_by_option
+        self.show_top_option = int(show_top_option)
+        print(group_by_option, group_by_option, show_top_option)
+        if group_by_option != "---":
+            if sort_by_option != "---":
+                self.group_and_sort()
+            else:
+                self.group_and_show_top()
+        else:
+            if sort_by_option != "---":
+                self.sort_and_show_top()
+
+    def sort(self):
+        if self.sort_by_option == "chrom1":
             self.all_contents.sort(
-                key=lambda x: float("inf")
+                key=lambda x: 23
                 if x.chrom1[3:] == "X"
+                else 24
+                if x.chrom1[3:] == "Y"
                 else int(x.chrom1[3:]),
                 reverse=False,
             )
-        if by == "chrom2":
+        if self.sort_by_option == "chrom2":
             self.all_contents.sort(
-                key=lambda x: float("inf")
+                key=lambda x: 23
                 if x.chrom2[3:] == "X"
+                else 24
+                if x.chrom2[3:] == "Y"
                 else int(x.chrom2[3:]),
                 reverse=False,
             )
-        if by == "sample":
+        if self.sort_by_option == "sample":
             self.all_contents.sort(
                 key=lambda x: int(x.sample[1:]),
                 reverse=False,
             )
-        if by == "score":
+        if self.sort_by_option == "score":
             self.all_contents.sort(key=lambda x: x.score, reverse=True)
-        if by == "start1":
+        if self.sort_by_option == "start1":
             self.all_contents.sort(key=lambda x: x.start1, reverse=True)
-        if by == "end1":
+        if self.sort_by_option == "end1":
             self.all_contents.sort(key=lambda x: x.end1, reverse=True)
-        if by == "start2":
+        if self.sort_by_option == "start2":
             self.all_contents.sort(key=lambda x: x.start2, reverse=True)
-        if by == "end2":
+        if self.sort_by_option == "end2":
             self.all_contents.sort(key=lambda x: x.end2, reverse=True)
-        if by == "score":
+        if self.sort_by_option == "score":
             self.all_contents.sort(key=lambda x: x.score, reverse=True)
 
-    def show_top(self, cols=10):
-        self.contents = self.all_contents[:cols]
+    def sort_and_show_top(self):
+        self.sort()
+        self.contents = self.all_contents[: self.show_top_option]
 
+    def group_and_show_top(self):
+        grouped_contents_dic = {}
+        for content in self.all_contents:
+            if self.group_by_option == "chrom1":
+                grouped_content = grouped_contents_dic.get(content.chrom1)
+                if grouped_content == None:
+                    grouped_contents_dic[content.chrom1] = []
+                elif self.show_top_option >= len(grouped_content):
+                    grouped_content.append(content)
+            if self.group_by_option == "chrom2":
+                grouped_content = grouped_contents_dic.get(content.chrom2)
+                if grouped_content == None:
+                    grouped_contents_dic[content.chrom2] = []
+                elif self.show_top_option >= len(grouped_content):
+                    grouped_content.append(content)
+            if self.group_by_option == "sample":
+                grouped_content = grouped_contents_dic.get(content.sample)
+                if grouped_content == None:
+                    grouped_contents_dic[content.sample] = []
+                elif self.show_top_option >= len(grouped_content):
+                    grouped_content.append(content)
+        self.contents = []
+        for grouped_content in grouped_contents_dic.values():
+            self.contents = [*self.contents, *grouped_content]
 
-SHOW_TOP = 10
+    def group_and_sort(self):
+        self.sort()
+        self.group_and_show_top()
 
 
 @homepage.route("/", methods=["GET", "POST"])
@@ -66,29 +137,27 @@ SHOW_TOP = 10
 def home() -> str | Response:
     from .database import File, Content
 
-    headers = filter(lambda h: "id" not in h, Content.__table__.columns.keys())
     file_contents = []
     files = File.query.filter_by(user_id=current_user.id)
 
     for f in files:
-        contents = []
+        all_contents = []
         for content in Content.query.filter_by(file_id=f.id):
-            contents.append(content)
-        file_content = FileData(f, headers, contents)
-        file_content.sort_by("chrom1")
-        file_content.show_top(SHOW_TOP)
-        file_contents.append(file_content)
+            all_contents.append(content)
+        file_contents.append(FileData(f, all_contents))
+
     if request.method == "POST":
         file = request.files["file"]
         content = validate_file(file)
         sortby_dropdown = request.form.get("sortby_dropdown")
+        groupby_dropdown = request.form.get("groupby_dropdown")
+        show_top = request.form.get("show_top")
 
         if content:
             return redirect(url_for("homepage.home"))
-        if sortby_dropdown:
+        if sortby_dropdown or groupby_dropdown or show_top:
             for f in file_contents:
-                f.sort_by(sortby_dropdown)
-                f.show_top(SHOW_TOP)
+                f.apply_settings_to_content(sortby_dropdown, groupby_dropdown, show_top)
             return render_template(
                 "homepage.html", user=current_user, file_contents=file_contents
             )
